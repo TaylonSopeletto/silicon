69b5314161306171b61e8b2c1574cab6702e337e
TaylonSopeletto <gtataylon@gmail.com>
2021-08-22 16:16:23 -0300

    fix templates

--- store/static/src/cart.js
+++ store/static/src/cart.js
@@ -1,11 +1,3 @@
 import './sass/style.scss';
 
-const removeButton = document.getElementById('removeProductButton')
 
-removeButton.addEventListener('click', () => {
-    removeProduct(removeButton.value)
-})
-
-const removeProduct = value => {
-    window.location.href = `/remove_product?id=${value}`
-}
--- store/static/src/main.js
+++ store/static/src/main.js
@@ -5,45 +5,43 @@ const currentParam = urlParams.get('order')
 
 const button = document.getElementById('price')
 
-if(button){
+if (button) {
     button.addEventListener('click', () => {
-    
+
         setFilter(button.value)
     })
-    
-    if(currentParam === 'price_asc') button.classList.add('up')
-    if(currentParam === 'price_desc') button.classList.remove()
+
+    if (currentParam === 'price_asc') button.classList.add('up')
+    if (currentParam === 'price_desc') button.classList.remove()
 }
 
 const setFilter = (value) => {
     const urlParams = new URLSearchParams(window.location.search)
     const currentParam = urlParams.get('order')
 
-    if(currentParam === `${value}_asc`){
+    if (currentParam === `${value}_asc`) {
         urlParams.set('order', `${value}_desc`)
-    }else{
+    } else {
         urlParams.set('order', `${value}_asc`)
     }
 
     window.location.search = urlParams
-    
+
 }
 
 /* add product to card */
 
-const rawProduct = JSON.parse(document.getElementById('product_data').textContent);
+const rawProduct = JSON.parse(document.getElementById('product_data')?.textContent);
 const jsonProduct = JSON.parse(rawProduct)
 
 const addProductButton = document.getElementById('addProductButton')
 const removeProductButton = document.getElementById('removeProductButton')
 
 addProductButton.addEventListener('click', () => {
-   window.location.href = `/add_product?id=${jsonProduct[0].pk}`
+    window.location.href = `/add_product?id=${jsonProduct[0].pk}`
 })
 
-removeProductButton.addEventListener('click', () => {
-    window.location.href = `/remove_product?id=${jsonProduct[0].pk}`
-})
+
 
 
 
